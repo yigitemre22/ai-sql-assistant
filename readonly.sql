6e93402563ee89404241c4d47a091c90d24a36a4
Yiğit Emre <emre2003yoruk@gmail.com>
2026-08-21 19:39:40 +0300

    Delete readonly.sql

--- readonly.sql
+++
@@ -1,7 +0,0 @@
-create role ai_readonly
-with LOGIN
-PASSWORD 'yigitemre1801';
-
-grant connect on database ai_sql_assistant to ai_readonly;
-grant usage on schema public to ai_readonly;
-grant select on table public.customers to ai_readonly;
